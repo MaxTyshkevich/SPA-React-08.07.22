d670bd6b5f2732dff67e95f123b0cec3fb0b5855
Max Tyshkevich <2842778@gmail.com>
2022-07-10 17:12:09 +0300

    feat:get and  render airport card

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import { AirportCard } from "../components/AirportCard";
 import { AirportFilter } from "../components/AirportFilter";
 import { AirportSearch } from "../components/AirportSearch";
+import { useAppDispatch, useAppSelector } from "../hook/redux";
+import { fetchAirport } from "../store/actions/actionsAction";
 
 const MainPage = () => {
+  const dispath = useAppDispatch();
+  const { airports, error, loading } = useAppSelector((state) => state.airport);
+
+  useEffect(() => {
+    dispath(fetchAirport());
+  }, []);
+
   return (
     <div className="container mx-auto max-w-[760px] pt-5">
       <AirportSearch />
       <AirportFilter />
-      <AirportCard />
+
+      {airports.map((airport) => (
+        <AirportCard key={airport.id} airport={airport} />
+      ))}
     </div>
   );
 };
