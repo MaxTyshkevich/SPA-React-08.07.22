d2c3eb3c501eef514fd5aaa27d2fe21f2c948b78
Max Tyshkevich <2842778@gmail.com>
2022-07-11 22:14:54 +0300

    feat: render card ditails

--- src/pages/AirportDetailPage.tsx
+++ src/pages/AirportDetailPage.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const AirportDetailPage = () => {
-  return <div>AirportDetailPage</div>;
+  const param = useParams<"id">();
+  return (
+    <div className="container mx-auto pt-5 max-w-[760px]">
+      <h1>Airport ID: {param.id}</h1>
+    </div>
+  );
 };
 
 export default AirportDetailPage;
